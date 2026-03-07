16721c649c157685402a0d52ac6fc06a822fc106
Karinna Alatorre <karinna98@csu.fullerton.edu>
2026-03-07 13:28:13 -0800

    Add BMI and Age to output folder.

---
+++ OUTPUT/FINAL Graphs_BMI_Age.R
@@ -0,0 +1,7 @@
+# Question 4.II
+
+hist(hints.data$Age, main = "histogram of age") # create histogram of 'Age' distribution
+
+boxplot (hints.data$BMI, main = "BMI Boxplot") # create boxplot of 'BMI'
+
+summary(hints.data$BMI) # provide statistical summary of 'BMI' 
