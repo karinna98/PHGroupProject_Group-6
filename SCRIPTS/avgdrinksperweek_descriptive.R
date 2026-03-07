a943bb68ac07ab1349ac7aeaf2c698c4849d2562
katrinahuynh12 <katrinahuynh12@csu.fullerton.edu>
2026-03-06 23:38:19 -0800

    avgdrinksperweek_descriptive.R

---
+++ SCRIPTS/avgdrinksperweek_descriptive.R
@@ -0,0 +1,22 @@
+# 'AvgDrinksPerWeek' B1
+
+avgdrinksperweek_descriptive <- summary(hints.data$AvgDrinksPerWeek, na.rm = TRUE) # Descriptive summary 'AvgDrinksPerWeek' variable
+avgdrinksperweek_descriptive
+
+# 'AvgDrinksPerWeek' B2.1 
+library(dplyr) # Open 'dplyr' package
+
+hints.data %>%
+  group_by(smokeStat) %>%
+  summarise(
+    mean_avgdrinksperweek = mean(AvgDrinksPerWeek, na.rm = TRUE), # Mean
+    median_avgdrinksperweek = median(AvgDrinksPerWeek, na.rm = TRUE), # Median 
+    sd_avgdrinksperweek = sd(AbgDrinksPerWeek, na.rm = TRUE), # Standard deviation
+    q25_avgdrinksperweek = quantile(AvgDrinksPerWeek, 0.25, na.rm = TRUE), # 25th percentile
+    q75_avgdrinksperweek = quantile(AvgDrinksPerWeek, 0.75, na.rm = TRUE) # 75th percentile
+  )
+
+# 'AvgDrinksPerWeek' B2.2
+library(skimr) # Open 'skimr' package
+
+skim(hints.data$AvgDrinksPerWeek) # Statistical summary using skimr
