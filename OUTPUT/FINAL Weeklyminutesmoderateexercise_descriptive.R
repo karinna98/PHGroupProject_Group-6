ad9cb47d6339b5a68b921d057fd33c4861dff8d4
jrosas24 <jrosas24013@gmail.com>
2026-03-08 12:49:02 -0700

    Update FINAL Weeklyminutesmoderateexercise_descriptive.R

--- OUTPUT/FINAL Weeklyminutesmoderateexercise_descriptive.R
+++ OUTPUT/FINAL Weeklyminutesmoderateexercise_descriptive.R
@@ -1,22 +1,35 @@
 # 'Weeklyminutesmoderateexercise' B1
 
-weeklyexercise_descriptive <- summary(hints.data$Weeklyminutesmoderateexercise, na.rm = TRUE) # Descriptive summary 'Weeklyminutesmoderateexercise' variable
-weeklyexercise_descriptive
+summary(hints.data$Weeklyminutesmoderateexercise) # Five-number summary and mean
+sd(hints.data$Weeklyminutesmoderateexercise, na.rm = TRUE) # Standard deviation
+var(hints.data$Weeklyminutesmoderateexercise, na.rm = TRUE) # Variance
 
-# 'Weeklyminutesmoderateexercise' B2.1 
+
+# 'Weeklyminutesmoderateexercise' B2.1
+
 library(dplyr) # Open 'dplyr' package
 
 hints.data %>%
   group_by(smokeStat) %>%
   summarise(
     mean_weeklyexercise = mean(Weeklyminutesmoderateexercise, na.rm = TRUE), # Mean
-    median_weeklyexercise = median(Weeklyminutesmoderateexercise, na.rm = TRUE), # Median 
     sd_weeklyexercise = sd(Weeklyminutesmoderateexercise, na.rm = TRUE), # Standard deviation
-    q25_weeklyexercise = quantile(Weeklyminutesmoderateexercise, 0.25, na.rm = TRUE), # 25th percentile
-    q75_weeklyexercise = quantile(Weeklyminutesmoderateexercise, 0.75, na.rm = TRUE) # 75th percentile
+    var_weeklyexercise = var(Weeklyminutesmoderateexercise, na.rm = TRUE) # Variance
   )
 
-# 'Weeklyminutesmoderateexercise' B2.2
-library(skimr) # Open 'skimr' package
 
-skim(hints.data$Weeklyminutesmoderateexercise) # Statistical summary using skimr
+# 'Weeklyminutesmoderateexercise' B2.2
+
+library(skimr) # Open 'skimr' package
+skim(hints.data$Weeklyminutesmoderateexercise)
+
+
+# B3 Function
+
+my_summary_func <- function(x){
+  summary(x)
+  sd(x, na.rm = TRUE)
+  var(x, na.rm = TRUE)
+}
+
+my_summary_func(hints.data$Weeklyminutesmoderateexercise)
